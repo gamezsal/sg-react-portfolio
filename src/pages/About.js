06697efecf3bd19c2340f80c208202f891d53b26
Sal Gamez <gamezsalvador@gmail.com>
2021-02-11 23:05:42 -0700

    Added about brief description

--- src/pages/About.js
+++ src/pages/About.js
@@ -20,47 +20,29 @@ function About() {
         <Row>
           <Col size="md-12">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-              aliquet diam tortor, id consequat mauris ullamcorper eu. Orci
-              varius natoque penatibus et magnis dis parturient montes, nascetur
-              ridiculus mus. Pellentesque et dui id justo finibus sollicitudin
-              at et metus. Ut feugiat tellus nec metus commodo, sed suscipit
-              nisi gravida. Duis eget vestibulum quam, ut porttitor sem. Donec
-              sagittis mi sollicitudin turpis semper, et interdum risus
-              lobortis. Vestibulum suscipit nunc non egestas tristique. Proin
-              hendrerit efficitur malesuada. Mauris lorem urna, sodales accumsan
-              quam non, tristique tempor erat. Nullam non sem facilisis, tempus
-              tortor sit amet, volutpat nisl. Ut et turpis non nunc maximus
-              mollis a vitae tortor. Pellentesque mattis risus ac quam laoreet
-              cursus. Praesent suscipit orci neque, vestibulum tincidunt augue
-              tincidunt non. Duis consequat mattis tortor vitae mattis.
+              “None of us are as smart as all of us” Dr. Ken Blanchard  
             </p>
             <p>
-              Phasellus at rutrum nisl. Praesent sed massa ut ipsum bibendum
-              porttitor. Sed malesuada molestie velit ac viverra. Quisque a
-              ullamcorper purus. Curabitur luctus mi ac mi hendrerit semper.
-              Nulla tincidunt accumsan lobortis. Mauris convallis sapien non
-              nibh porta accumsan. Nunc volutpat tempus porttitor. Nunc congue
-              dictum egestas. Aliquam blandit mi eu urna scelerisque, vitae
-              volutpat ligula ultricies. Maecenas vel porta augue. Fusce mauris
-              ex, dignissim et lacinia ut, tempus eget nibh.
+            My name
+              is Sal, I'm a customer advocate who enjoys solving complex
+              problems. Several years experience in agile development,
+              experience working with engineering teams building and releasing
+              great products to the market. Certifications in SAFe Agile
+              practices as a Product Manager as well as a Certified Scrum
+              Product Owner (CSPO). Have led multiple engineering teams acting
+              as the voice of the customers within the Software Development Life
+              Cycle process. Familiar with conducting Sprint Planning meeting’s,
+              Sprint Review’s, Sprint Retrospective’s, and Sprint Demos. A
+              person that can translate business needs into actionable stories
+              that can be worked on by the engineering team, I enjoy learning
+              and serving other’s in the process.
             </p>
             <p>
-              Etiam ut massa efficitur, gravida sapien non, condimentum sapien.
-              Suspendisse massa tortor, facilisis in neque sit amet, scelerisque
-              elementum tortor. Nullam eget nibh sit amet odio lobortis
-              ullamcorper. Nulla bibendum magna nec sem pulvinar lobortis.
-              Mauris et imperdiet urna, vitae lobortis dui. Nunc elementum elit
-              mi, non mattis enim congue at. Proin mi lectus, ullamcorper id
-              hendrerit eu, ultricies vitae lacus. Nunc vehicula, erat eget
-              laoreet condimentum, felis ante malesuada leo, nec efficitur diam
-              nisi eget nisi. Cras arcu lacus, tristique in bibendum vitae,
-              elementum eget lorem. Maecenas vestibulum volutpat orci eu
-              pharetra. Praesent vel blandit ante, nec faucibus libero. Sed
-              ultrices lorem ex, eu facilisis libero convallis ac. Vivamus id
-              dapibus eros. Nullam tempor sem rhoncus porta semper. Proin
-              bibendum vulputate nisl, fringilla interdum elit pulvinar eu.
-              Quisque vitae quam dapibus, vestibulum mauris quis, laoreet massa.
+            On a personal note, I'm family
+              man and a strong believer of a work/life balance. God and Family
+              play a vital role in my life. Do you know of any available opportunities to work in
+              development?” Hit me up! I'm looking for my next
+              opportunity.
             </p>
           </Col>
         </Row>
