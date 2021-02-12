b5f8669306dc9516268bf3d8eec28c959c3f72f4
Sal Gamez <gamezsalvador@gmail.com>
2021-02-11 22:58:01 -0700

    build out col, container, footer, navbar, pages for porfolio, handle the about route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./pages/About"
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+  
+// import logo from './logo.svg';
+// import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          {/* <Route exact path="/projects" component={Projects} />
+          <Route exact path="/personal" component={Personal} /> */}
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
